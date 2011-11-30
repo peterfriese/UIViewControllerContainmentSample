714b2371f95186ef9416f20981f6b70980e01b29
Peter Friese <peter@peterfriese.de>
2011-11-30 17:42:46 +0100

    Initial Commit

---
+++ UIViewControllerContainmentSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIViewControllerContainmentSample
+//
+//  Created by Peter Friese on 30.11.11.
+//  Copyright (c) 2011 http://peterfriese.de. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
